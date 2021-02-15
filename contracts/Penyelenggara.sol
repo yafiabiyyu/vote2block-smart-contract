0ff87b342c79fdcca27fb2097c67c115bd20b192
Abiyyu Yafi <luckyveri3@gmail.com>
2021-02-15 13:53:22 +0700

    Merubah semua data dari public menjadi internal

--- contracts/Penyelenggara.sol
+++ contracts/Penyelenggara.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Penyelenggara is AccessControl {
 
-    bytes32 public constant DEFAULT_ROLE_PETUGAS = keccak256("DEFAULT_ROLE_PETUGAS");
-    bytes32 public constant DEFAULT_ADMIN_PETUGAS = keccak256("DEFAULT_ADMIN_PETUGAS");
+    bytes32 internal constant DEFAULT_ROLE_PETUGAS = keccak256("DEFAULT_ROLE_PETUGAS");
+    bytes32 internal constant DEFAULT_ADMIN_PETUGAS = keccak256("DEFAULT_ADMIN_PETUGAS");
 
     constructor(address _ketuaAddress) {
         _setupRole(DEFAULT_ADMIN_ROLE, _ketuaAddress);
@@ -28,22 +28,22 @@ contract Penyelenggara is AccessControl {
         _;
     }
     
-    function setAdminPetugasAddress(address _adminPetugasAddress) public onlyOwner {
+    function setAdminPetugasAddress(address _adminPetugasAddress) internal onlyOwner {
         grantRole(DEFAULT_ADMIN_PETUGAS, _adminPetugasAddress);
         emit RoleGranted(DEFAULT_ADMIN_PETUGAS, _adminPetugasAddress, msg.sender);
     }
     
-    function removeAdminPetugasAddress(address _adminPetugasAddress) public onlyOwner {
+    function removeAdminPetugasAddress(address _adminPetugasAddress) internal onlyOwner {
         revokeRole(DEFAULT_ADMIN_PETUGAS, _adminPetugasAddress);
         emit RoleRevoked(DEFAULT_ADMIN_PETUGAS, _adminPetugasAddress, msg.sender);
     }
     
-    function addNewPetugas(address _petugasAddress) public onlyAdminPetugas {
+    function addNewPetugas(address _petugasAddress) internal onlyAdminPetugas {
         grantRole(DEFAULT_ROLE_PETUGAS, _petugasAddress);
         emit RoleGranted(DEFAULT_ROLE_PETUGAS, _petugasAddress, msg.sender);
     }
     
-    function removePetugas(address _petugasAddress) public onlyAdminPetugas {
+    function removePetugas(address _petugasAddress) internal onlyAdminPetugas {
         revokeRole(DEFAULT_ROLE_PETUGAS, _petugasAddress);
         emit RoleRevoked(DEFAULT_ROLE_PETUGAS, _petugasAddress, msg.sender);
     }
