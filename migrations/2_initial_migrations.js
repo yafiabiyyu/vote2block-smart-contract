4fbd0febf9703662285f7793c18832142fb5d0ec
Abiyyu Yafi <luckyveri3@gmail.com>
2021-03-22 16:27:51 +0700

    update migrations file

--- migrations/2_initial_migrations.js
+++ migrations/2_initial_migrations.js
@@ -1,5 +1,5 @@
 const Vote2Block = artifacts.require("Vote2Block");
 
 module.exports = function (deployer, networks, accounts) {
-  deployer.deploy(Vote2Block,accounts[0]);
+  deployer.deploy(Vote2Block,accounts[1]);
 };
