d43b1b8ece71c195fb65342c796658e3036b9535
Abiyyu Yafi <luckyveri3@gmail.com>
2021-04-24 11:38:25 +0700

    remove AdminPetugas

--- migrations/2_initial_migrations.js
+++ migrations/2_initial_migrations.js
@@ -1,5 +1,5 @@
 const Vote2Block = artifacts.require("Vote2Block");
 
 module.exports = function (deployer, networks, accounts) {
-  deployer.deploy(Vote2Block,accounts[1]);
+  deployer.deploy(Vote2Block,"0x0369FCBDaDB6E9AcE265a1895114e261Df41248d");
 };
